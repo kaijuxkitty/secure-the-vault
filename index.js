511681393baba54e12f305afa653d5881a10729e
kaijuxkitty <kaijuxkitty@gmail.com>
2024-10-14 16:51:49 -0500

    popup

---
+++ index.js
@@ -0,0 +1,21 @@
+//create a string with a message for user with combination: 10-40-39
+//create 3 variables with result that equals each number of combination
+//alert user of combination 
+
+const message = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:';
+
+const combination = "10-40-39"
+
+alert(message +combination);
+
+c
+
+
+
+
+
+
+
+
+
+
